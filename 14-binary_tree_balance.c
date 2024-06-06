8d3bc8115b7cfc2b21487ab2ba80a8cea5d3b83b
NdivhoNemz <ndivhonematandani@gmail.com>
2024-06-06 23:27:47 +0200

    Balance factor fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,8 +27,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t left_side = 0, right_side = 0;
 
-		left_side = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		right_side = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		left_side = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		right_side = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((left_side > right_side) ? left_side : right_side);
 	}
 	return (0);
